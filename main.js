9669731a3f71de34c18027c8c52bf7d7186fe64b
imyeremin <eremipsch@mail.ru>
2024-08-10 17:03:31 +0300

    new date for five days

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const apiKey = "f86a408c1e5a6ebd1e5c569b56031e69";
 // const apiURL = `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${apiKey}`;
 let days = null;
 let dataFeatch = [];
-const arrDays = ["1", "2", "3", "4", "5", "6", "7"];
+const arrDays = [];
 window.onload = async () => {
   const startData = await run("Рязань");
   const startData2 = await run2("Рязань");
@@ -40,7 +40,8 @@ const run = async (city) => {
   let resp = null;
   try {
     resp = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    );
   } catch (e) {
     const container = document.getElementById("container");
     container.innerHTML = `
@@ -56,7 +57,8 @@ const run2 = async (city) => {
   let resp = null;
   try {
     resp = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    );
   } catch (e) {
     const container = document.getElementById("container");
     container.innerHTML = `
@@ -249,77 +251,121 @@ const btnToday = document.getElementById("btn-today");
 const btnDays = document.getElementById("btn-days");
 
 btnDays.onclick = async () => {
-  creatingPagesDays(dataFeatch[0], dataFeatch[1]);
-  initDays(dataFeatch[0]);
+  creatingPagesDays(dataFeatch[0]);
+  const arrData =  fillingWithData(dataFeatch[0])
+  console.log(arrData)
+  initDays(arrData);
 };
 btnToday.onclick = () => {
   creatingPages(dataFeatch[0], dataFeatch[1]);
 };
 
-const creatingPagesDays = (data, dataNow) => {
+const creatingPagesDays = (data) => {
   const containerDays = document.getElementById("container-days");
   container.innerHTML = ` <div class="container p-3 mt-3 mb-3 d-flex">
         <div class="card day" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title head">TONIGHT</h5>
-            <h6 class="card-text">JUN 30</h6>
-            <img src="..." class="card-img-top" alt="..." />
-            <h3 class="card-title">25 C</h3>
-            <p class="card-text">Clear, Warm</p>
+            <h5 class="card-title head">Today</h5>
+            <h6 class="card-text"> ${new Date(
+              data.list[0].dt * 1000
+            ).toLocaleDateString()}</h6>
+            <img width="100px" src="http://openweathermap.org/img/w/${
+              data.list[0].weather[0].icon
+            }.png" alt="" />
+            <h3 class="card-title">${Math.floor(
+              data.list[0].main.temp - 273
+            )}°C</h3>
+            <p class="card-text">${data.list[0].weather[0].main}</p>
           </div>
         </div>
         <div class="card day" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title head">TONIGHT</h5>
-            <h6 class="card-text">JUN 30</h6>
-            <img src="..." class="card-img-top" alt="..." />
-            <h3 class="card-title">25 C</h3>
-            <p class="card-text">Clear, Warm</p>
+            <h5 class="card-title head">The next hours</h5>
+            <h6 class="card-text">${new Date(
+              data.list[6].dt * 1000
+            ).toLocaleDateString()}</h6>
+            <img width="100px" src="http://openweathermap.org/img/w/${
+              data.list[6].weather[0].icon
+            }.png" alt="" />
+            <h3 class="card-title">${Math.floor(
+              data.list[6].main.temp - 273
+            )}°C</h3>
+            <p class="card-text">${data.list[6].weather[0].main}</p>
           </div>
         </div>
         <div class="card day" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title head">TONIGHT</h5>
-            <h6 class="card-text">JUN 30</h6>
-            <img src="..." class="card-img-top" alt="..." />
-            <h3 class="card-title">25 C</h3>
-            <p class="card-text">Clear, Warm</p>
+            <h5 class="card-title head">The next hours</h5>
+            <h6 class="card-text">${new Date(
+              data.list[12].dt * 1000
+            ).toLocaleDateString()}</h6>
+            <img width="100px" src="http://openweathermap.org/img/w/${
+              data.list[12].weather[0].icon
+            }.png" alt="" />
+            <h3 class="card-title">${Math.floor(
+              data.list[12].main.temp - 273
+            )}°C</h3>
+            <p class="card-text">${data.list[12].weather[0].main}</p>
           </div>
         </div>
         <div class="card day" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title head">TONIGHT</h5>
-            <h6 class="card-text">JUN 30</h6>
-            <img src="..." class="card-img-top" alt="..." />
-            <h3 class="card-title">25 C</h3>
-            <p class="card-text">Clear, Warm</p>
+            <h5 class="card-title head">The next hours</h5>
+            <h6 class="card-text">${new Date(
+              data.list[18].dt * 1000
+            ).toLocaleDateString()}</h6>
+            <img width="100px" src="http://openweathermap.org/img/w/${
+              data.list[18].weather[0].icon
+            }.png" alt="" />
+            <h3 class="card-title">${Math.floor(
+              data.list[18].main.temp - 273
+            )}°C</h3>
+            <p class="card-text">${data.list[18].weather[0].main}</p>
           </div>
         </div>
         <div class="card day" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title head">TONIGHT</h5>
-            <h6 class="card-text">JUN 30</h6>
-            <img src="..." class="card-img-top" alt="..." />
-            <h3 class="card-title">25 C</h3>
-            <p class="card-text">Clear, Warm</p>
+            <h5 class="card-title head">The next hours</h5>
+            <h6 class="card-text">${new Date(
+              data.list[24].dt * 1000
+            ).toLocaleDateString()}</h6>
+            <img width="100px" src="http://openweathermap.org/img/w/${
+              data.list[24].weather[0].icon
+            }.png" alt="" />
+            <h3 class="card-title">${Math.floor(
+              data.list[24].main.temp - 273
+            )}°C</h3>
+            <p class="card-text">${data.list[24].weather[0].main}</p>
           </div>
         </div>
         <div class="card day" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title head">TONIGHT</h5>
-            <h6 class="card-text">JUN 30</h6>
-            <img src="..." class="card-img-top" alt="..." />
-            <h3 class="card-title">25 C</h3>
-            <p class="card-text">Clear, Warm</p>
+            <h5 class="card-title head">The next hours</h5>
+            <h6 class="card-text">${new Date(
+              data.list[30].dt * 1000
+            ).toLocaleDateString()}</h6>
+            <img width="100px" src="http://openweathermap.org/img/w/${
+              data.list[30].weather[0].icon
+            }.png" alt="" />
+            <h3 class="card-title">${Math.floor(
+              data.list[30].main.temp - 273
+            )}°C</h3>
+            <p class="card-text">${data.list[30].weather[0].main}</p>
           </div>
         </div>
         <div class="card day" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title head">TONIGHT</h5>
-            <h6 class="card-text">JUN 30</h6>
-            <img src="..." class="card-img-top" alt="..." />
-            <h3 class="card-title">25 C</h3>
-            <p class="card-text">Clear, Warm</p>
+            <h5 class="card-title head">The next hours</h5>
+            <h6 class="card-text">${new Date(
+              data.list[36].dt * 1000
+            ).toLocaleDateString()}</h6>
+            <img width="100px" src="http://openweathermap.org/img/w/${
+              data.list[36].weather[0].icon
+            }.png" alt="" />
+            <h3 class="card-title">${Math.floor(
+              data.list[36].main.temp - 273
+            )}°C </h3>
+            <p class="card-text">${data.list[36].weather[0].main}</p>
           </div>
         </div>
       </div>
@@ -333,61 +379,97 @@ const creatingPagesDays = (data, dataNow) => {
               <thead>
                 <tr>
                   <th scope="col">TODAY</th>
-                  <th scope="col">1</th>
-                  <th scope="col">1</th>
-                  <th scope="col">1</th>
-                  <th scope="col">1</th>
-                  <th scope="col">1</th>
-                  <th scope="col">1</th>
+                  <th scope="col">${new Date(
+                    data.list[0].dt * 1000
+                  ).toLocaleTimeString()}</th>
+                  <th scope="col">${new Date(
+                    data.list[1].dt * 1000
+                  ).toLocaleTimeString()}</th>
+                  <th scope="col">${new Date(
+                    data.list[2].dt * 1000
+                  ).toLocaleTimeString()}</th>
+                  <th scope="col">${new Date(
+                    data.list[3].dt * 1000
+                  ).toLocaleTimeString()}</th>
+                  <th scope="col">${new Date(
+                    data.list[4].dt * 1000
+                  ).toLocaleTimeString()}</th>
+                  <th scope="col">${new Date(
+                    data.list[5].dt * 1000
+                  ).toLocaleTimeString()}1</th>
                 </tr>
               </thead>
               <tbody class="text-center">
                 <tr>
                   <th scope="row"></th>
-                  <td><img width="100px" src="" alt="" /></td>
-                  <td><img width="100px" src="" alt="" /></td>
-                  <td><img width="100px" src="" alt="" /></td>
-                  <td><img width="100px" src="" alt="" /></td>
-                  <td><img width="100px" src="" alt="" /></td>
-                  <td><img width="100px" src="" alt="" /></td>
+                  <td> <img width="100px" src="http://openweathermap.org/img/w/${
+                    data.list[0].weather[0].icon
+                  }.png" alt="" /></td>
+                  <td> <img width="100px" src="http://openweathermap.org/img/w/${
+                    data.list[1].weather[0].icon
+                  }.png" alt="" /></td>
+                  <td> <img width="100px" src="http://openweathermap.org/img/w/${
+                    data.list[2].weather[0].icon
+                  }.png" alt="" /></td>
+                  <td> <img width="100px" src="http://openweathermap.org/img/w/${
+                    data.list[3].weather[0].icon
+                  }.png" alt="" /></td>
+                  <td> <img width="100px" src="http://openweathermap.org/img/w/${
+                    data.list[4].weather[0].icon
+                  }.png" alt="" /></td>
+                  <td> <img width="100px" src="http://openweathermap.org/img/w/${
+                    data.list[5].weather[0].icon
+                  }.png" alt="" /></td>
                 </tr>
 
                 <tr>
                   <th class="fw-light" scope="row">Forecast</th>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
+                  <td>${data.list[0].weather[0].main}</td>
+                  <td>${data.list[1].weather[0].main}</td>
+                  <td>${data.list[2].weather[0].main}</td>
+                  <td>${data.list[3].weather[0].main}</td>
+                  <td>${data.list[4].weather[0].main}</td>
+                  <td>${data.list[5].weather[0].main}</td>
                 </tr>
 
                 <tr>
                   <th class="fw-light" scope="row">Temp(°C)</th>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
+                        <td>${Math.floor(data.list[0].main.temp - 273)}°C</td>
+                        <td>${Math.floor(data.list[1].main.temp - 273)}°C</td>
+                        <td>${Math.floor(data.list[2].main.temp - 273)}°C</td>
+                        <td>${Math.floor(data.list[3].main.temp - 273)}°C</td>
+                        <td>${Math.floor(data.list[4].main.temp - 273)}°C</td>
+                        <td>${Math.floor(data.list[5].main.temp - 273)}°C</td>
                 </tr>
                 <tr>
                   <th class="fw-light" scope="row">RealFeel</th>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
+                  <td>${Math.floor(data.list[0].main.feels_like - 273)} °C</td>
+                  <td>${Math.floor(data.list[1].main.feels_like - 273)} °C</td>
+                  <td>${Math.floor(data.list[2].main.feels_like - 273)} °C</td>
+                  <td>${Math.floor(data.list[3].main.feels_like - 273)} °C</td>
+                  <td>${Math.floor(data.list[4].main.feels_like - 273)} °C</td>
+                  <td>${Math.floor(data.list[5].main.feels_like - 273)} °C</td>
                 </tr>
                 <tr>
                   <th class="fw-light" scope="row">Wind(km/h)</th>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
-                  <td>1</td>
+                  <td>${
+                    (Math.floor(data.list[0].wind.speed) * 3600) / 1000
+                  }</td>
+                  <td>${
+                    (Math.floor(data.list[1].wind.speed) * 3600) / 1000
+                  }</td>
+                  <td>${
+                    (Math.floor(data.list[2].wind.speed) * 3600) / 1000
+                  }</td>
+                  <td>${
+                    (Math.floor(data.list[3].wind.speed) * 3600) / 1000
+                  }</td>
+                  <td>${
+                    (Math.floor(data.list[4].wind.speed) * 3600) / 1000
+                  }</td>
+                  <td>${
+                    (Math.floor(data.list[5].wind.speed) * 3600) / 1000
+                  }</td>
                 </tr>
               </tbody>
             </table>
@@ -402,7 +484,7 @@ const initDays = (data) => {
   days.forEach((day, i) => {
     day.onclick = () => {
       contentDay.innerHTML = `<div class="head fs-5 p-1 d-flex justify-content-between">
-        <div>HOURLY</div>
+        <div>${i}</div>
       </div>
       <div class="content d-flex alight-item-center justify-content-around">
         <div class="container">
@@ -499,6 +581,22 @@ const initDays = (data) => {
 };
 
 // TEST
-
-
+const fillingWithData = (data) => {
+  let arrPush = [];
+  let arrData = [];
+  console.log(data)
+  for (let i = 0; i < data.list.length; i++) {
+    arrPush.push(data.list[i]);
+    if (i == data.list.length - 1) {
+      arrData.push(arrPush);
+      break;
+    }
+    if (arrPush.length >= 6) {
+      arrData.push(arrPush);
+      arrPush = [];
+    }
+  }
+  console.log(arrData)
+  return arrData;
+};
 // TEST
